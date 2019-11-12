ea53e5f27050a571ca935b77ab98895650d535a3
Ripon Babu <riponbabu392@gmail.com>
2019-11-12 10:29:16 +0530

    presentational component and conatainer component

--- src/components/ComponentMenu.js
+++ src/components/ComponentMenu.js
@@ -1,38 +1,16 @@
 import React,{Component} from 'react';
 import {Card,CardTitle,CardImg,CardImgOverlay} from 'reactstrap';
-import DishDetail from './DishdetailComponent';
+
 
 class Menu extends Component{
 
-    constructor(props){
-        super(props);
-        this.state={
-            selectedDish:null,
-        };  
-         
-    }
-
-    onDishSelected(dish){
-        this.setState({selectedDish:dish});
-    }
-
-    renderDish(dish) {
-        if (dish != null)
-            return(
-               <DishDetail selectedDish={dish}/>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
 
     render(){
        
         const menu=this.props.dishes.map((dish)=>{
               return(
                   <div className="col-12 col-md-5 m-1">
-                     <Card key={dish.id} onClick={()=>this.onDishSelected(dish)}>
+                     <Card key={dish.id} onClick={()=>this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                           <CardTitle>{dish.name}</CardTitle>
@@ -42,13 +20,10 @@ class Menu extends Component{
               );
         });
         return(
-            <div className="continer">
+            <div className="container ">
                <div className="row">
                   {menu}
-               </div>
-              
-                 {this.renderDish(this.state.selectedDish)}
-               
+               </div>  
             </div>
         );
     }
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,37 +3,64 @@ import {Card,CardBody,CardText,CardTitle,CardImg} from 'reactstrap';
 
 class DishDetail extends Component{
 
-    render(){
-      
-        const comment=this.props.selectedDish.comments.map((comment)=>{
-            return(
-               <div key={comment.id}>
-                 <p>{comment.comment}</p>
-                 <p>-- {comment.author} , {comment.date}</p>
-               </div>
-            )
-        });
-        return(
-           <div className="row">
+
+   renderDish(dish){
+      if(dish===undefined){
+         return <div></div>
+      }
+      else{
+         return <DishShow dish={dish}/>
+      }
+   }
+
+   render(){
+     const dish=this.props.dish;
+     return(
+        <div className="container " >
+          
+            {this.renderDish(dish)}
+         
+        </div>
+          
+     );
+
+   }
+}
+
+class DishShow extends Component{
+
+   render(){
+     
+       const comment=this.props.dish.comments.map((comment)=>{
+          return (
+             <div key={comment.id}>
+               <p>{comment.comment}</p>
+               <p> -- {comment.author} , {new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+             </div>
+          )
+       })
+
+
+      return(
+             <div className="row"> 
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={this.props.selectedDish.image}  alt={this.props.selectedDish.name}/>
-                        <CardBody>
-                            <CardTitle>{this.props.selectedDish.name}</CardTitle>
-                            <CardText>{this.props.selectedDish.description}</CardText>
-                        </CardBody>
+                       <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                       <CardBody>
+                         <CardTitle>{this.props.dish.name}</CardTitle>
+                         <CardText>{this.props.dish.description}</CardText>
+                       </CardBody>
                     </Card>
-                    
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    <h3>comments :</h3>
-                    {comment}
+                     <h3>comments:</h3>
+                     {comment}
                 </div>
-
-           </div>
-        );
-    }
+            </div>
+      );
+   }
 }
+
 
 
 export default DishDetail;
