c4bc49ac96caf50c8d4092d9c03deaae7a1470d4
Ripon Babu <riponbabu392@gmail.com>
2019-11-16 10:19:45 +0530

    Router first time

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,14 +1,54 @@
 import React,{Component} from 'react';
-import {Navbar,NavbarBrand,Jumbotron} from 'reactstrap';
+import {Navbar,NavbarBrand,Jumbotron,Nav,NavbarToggler,Collapse,NavItem} from 'reactstrap';
+import {NavLink} from 'react-router-dom';
 
 class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isNavOpen:false
+        }
+        this.navToggle=this.navToggle.bind(this);
+    }
+
+    navToggle(){
+        this.setState({isNavOpen:!this.state.isNavOpen});
+    }
+
     render(){
         return(
            <React.Fragment>
-                <Navbar className="navbar-dark">
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">Ristorent con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.navToggle} />
+                        <NavbarBrand className=" mr-auto" href="/">
+                          <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+                        </NavbarBrand>
                     </div>
+                   <Collapse navbar isOpen={this.state.isNavOpen}> 
+                    <Nav navbar className="mr-auto" >
+                      <NavItem >
+                         <NavLink className="nav-link" to="/home">
+                             <span className="fa fa-home fa-lg"></span> Home 
+                         </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink className="nav-link" to="/aboutus">
+                            <span className="fa fa-info fa-lg"></span> About us 
+                        </NavLink>
+                     </NavItem>
+                    <NavItem>
+                            <NavLink className="nav-link" to="/menu">
+                                <span className="fa fa-list fa-lg"></span> Menu
+                            </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className="nav-link" to="/contactus">
+                           <span className="fa fa-address-card fa-lg"></span> Contact Us
+                        </NavLink>
+                    </NavItem>
+                   </Nav>
+                  </Collapse>
                 </Navbar>
                 <Jumbotron>
                   <div className="container">
---
+++ src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Home(props) {
+    return(
+      <div className="container">
+        <h1>hello</h1>
+      </div>
+    );
+}
+
+export default Home; 
