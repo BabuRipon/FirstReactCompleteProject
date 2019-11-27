594c71c7cd8fd2057fd7b41ea736dcccbee9b498
Ripon Babu <riponbabu392@gmail.com>
2019-11-27 16:15:30 +0530

    Reducer combine

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,18 @@
-import{createStore}from 'redux';
-import{Reducer,initialState}from './reducer';
+import{createStore,combineReducers}from 'redux';
+import{Dishes}from './dishes';
+import{Comments}from './comments';
+import{Leaders}from './leaders';
+import{Promotions}from './promotions';
 
 export const configureStore=()=>{
-    const store=createStore(
-        Reducer,
-        initialState
-    );
-    return store;
+   const store=createStore(
+       combineReducers({
+           dishes:Dishes,
+           promotions:Promotions,
+           comments:Comments,
+           leaders:Leaders,
+       })
+   )
+
+   return store;
 }
